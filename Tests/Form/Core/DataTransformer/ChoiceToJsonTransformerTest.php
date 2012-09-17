4b6b4dfbcf11aa20bcba88a5d8b6c9a9055524dc
Pierre Penninckx <ibizapeanut@gmail.com>
2012-09-17 23:29:22 +0200

    adds test and explicit error message in case of misuse of multiple option

---
+++ Tests/Form/Core/DataTransformer/ChoiceToJsonTransformerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Olivier Chauvel <olivier@generation-multiple.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Genemu\Bundle\FormBundle\Tests\Core\DataTransformer;
+
+use Symfony\Component\Form\Extension\Core\ChoiceList\SimpleChoiceList;
+use Genemu\Bundle\FormBundle\Form\Core\DataTransformer\ChoiceToJsonTransformer;
+
+class ChoiceToJsonTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Genemu\Bundle\FormBundle\Exception\WrongUsageOfOption
+     */
+    public function testReverseMultipleShouldBeFalse()
+    {
+        $list = new SimpleChoiceList(array(
+            'label' => 'Foo', 'value' => 'foo'
+        ));
+        $transformer = new ChoiceToJsonTransformer(
+            $list,
+            false,
+            'choice',
+            true
+        );
+
+        $transformer->reverseTransform('{"label": "Foo", "value": "foo"}');
+    }
+
+    /**
+     * @expectedException Genemu\Bundle\FormBundle\Exception\WrongUsageOfOption
+     */
+    public function testReverseMultipleShouldBeTrue()
+    {
+        $list = new SimpleChoiceList(array(
+            'label' => 'Foo', 'value' => 'foo'
+        ));
+        $transformer = new ChoiceToJsonTransformer(
+            $list,
+            false,
+            'choice',
+            false
+        );
+
+        $transformer->reverseTransform('[{"label": "Foo", "value": "foo"}]');
+    }
+}
